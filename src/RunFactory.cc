75929e428969f36af96c43ddf98824275d7b4cfa
cmsbuild <>
2010-02-18 20:56:17 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats- FWLite-V01-03-00'.
    Sprout from master 2010-02-18 20:56:13 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Convert BuildFiles to XML'
    Cherrypick from master 2010-02-18 20:44:58 UTC Eric Wayne Vaandering <sha1-87df1b4d94f961dadccf9690dda6dfaf3f30ca0c@cern.ch> 'Use RunFactory to avoid creating two Run objects':
        src/RunFactory.cc

--- src/RunFactory.cc
+++ src/RunFactory.cc
@@ -8,13 +8,20 @@
 //
 // Original Author:
 //         Created:  Wed Feb 10 11:15:18 CST 2010
-// $Id: RunFactory.cc,v 1.1 2010/02/18 20:44:58 ewv Exp $
+// $Id: HistoryGetterBase.cc,v 1.1 2010/02/11 17:21:39 ewv Exp $
 //
 
 // system include files
 
 // user include files
 #include "DataFormats/FWLite/interface/RunFactory.h"
+
+
+namespace {
+  struct NoDelete {
+    void operator()(void*){}
+  };
+}
 
 namespace fwlite {
 
